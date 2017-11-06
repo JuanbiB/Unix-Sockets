78ef4dffb745328ee2ace629488253b251e9cd5e
JuanbiB <juanbiiberretta@gmail.com>
2017-11-06 18:17:46 -0500

    Cleanup

--- crc_generator.cc
+++ crc_generator.cc
@@ -6,39 +6,6 @@
 #include <stdint.h>
 
 using namespace std;
-
-uint16_t getCRC(char * message, int len){
-  uint16_t crc = 0x0000;
-  uint16_t G = 0x8005;
-
-  for (int i = 0; i < len; i++){
-    uint8_t byte = message[i];
-    for (int j = 7; j >= 0; j--){
-      if ((crc & 0x8000) != 0){ // If MSB is set in CRC code.
-	crc = crc << 1;
-	if ((byte & (1 << j)) != 0){
-	  crc |= 0x0001; // Set LSB to 1
-	}
-	else {
-	  crc |= 0x0000; // Set LSB to 0
-	}
-	crc ^=  G;
-      }
-      else {
-	crc = crc << 1;
-	if ((byte & (1<<j)) != 0){
-	  crc = crc |= 0x0001; // Set LSB to 1
-	}
-	else {
-	  crc = crc |= 0x0000; // Set LSB to 0
-	}
-      }
-    }
-  }
-  printf("\n0x%x\n", crc);
-  return crc;
-}
-
 int getCRC2(char * message, int len){
     unsigned short crc = 0x0000;
     unsigned short generator = 0x8005;
@@ -57,18 +24,5 @@ int getCRC2(char * message, int len){
     }
     return crc;
 }
-
-// int main() {
-//   char* buf = "abcd";
-//   char* buf2 = "abcd00";
-//   getCRC(buf2, 6);
-//   uint16_t crc2 = getCRC2(buf, 4);
-//   int left = crc2 & 0xFF00;
-//   int right = crc2 & 0xFF;
-//   printf("\n0x%x\n", left + right);
-//   printf("\n0x%x\n", right);
-//       //  crc3(buf, 6);
-//   return 0;
-// }
 
 
